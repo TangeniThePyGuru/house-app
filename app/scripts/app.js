3051a5f669168e7bd722a40e85bc0c5feb14aebc
Tangeni <tangenishikomba@gmail.com>
2017-10-07 15:14:54 +0200

    initial commit

---
+++ app/scripts/app.js
@@ -0,0 +1,20 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name myHouseAppApp
+ * @description
+ * # myHouseAppApp
+ *
+ * Main module of the application.
+ */
+angular.module('myHouseAppApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch',
+    'firebase',
+    'firebase.Auth'
+  ]);
