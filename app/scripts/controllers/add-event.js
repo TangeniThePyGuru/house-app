cd65c16d6e6704a8a00ff35795d905686290f1c8
Tangeni <tangenishikomba@gmail.com>
2017-10-16 02:15:36 +0200

    add event controller added.

--- app/scripts/controllers/add-event.js
+++ app/scripts/controllers/add-event.js
@@ -8,7 +8,7 @@
  * Controller of the myHouseAppApp
  */
 angular.module('myHouseAppApp')
-  .controller('AddEventCtrl', ["auth", "$scope", "$location", function (auth, $scope, $location) {
+  .controller('AddEventCtrl', ["auth", "$scope", "$location","eventsFactory","$timeout", function (auth, $scope, $location, eventsFactory, $timeout) {
     $scope.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
@@ -21,5 +21,33 @@ angular.module('myHouseAppApp')
       $location.path('/login');
       $scope.authData = null;
     };
+
+    $scope.success = false;
+    $scope.notsuccess = false;
+
+    $scope.add = function () {
+      console.log($scope.starting_date + " -> "+ $scope.starting_time)
+      eventsFactory.add({
+        title : $scope.title,
+        starting_date : $scope.starting_date,
+        starting_time: $scope.starting_time,
+        ending_date : $scope.ending_date,
+        ending_time: $scope.ending_time,
+        description: $scope.description
+      }).then(function (ref) {
+        console.log("success: "+ref)
+        $scope.success = true;
+        $timeout(function () {
+          $scope.success = false;
+        }, 3000)
+      }, function (error) {
+        console.log("error: "+error);
+        $scope.notsuccess = true;
+        $timeout(function () {
+          $scope.notsuccess = false;
+        }, 3000)
+      });
+
+    }
 
   }]);
