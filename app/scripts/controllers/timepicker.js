207d5b64aed389e0a1cfa63bd6d6b1fcf01a3362
Tangeni <tangenishikomba@gmail.com>
2017-10-16 02:42:29 +0200

    timepicker files added

---
+++ app/scripts/controllers/timepicker.js
@@ -0,0 +1,50 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name myHouseAppApp.controller:TimepickerCtrl
+ * @description
+ * # TimepickerCtrl
+ * Controller of the myHouseAppApp
+ */
+angular.module('myHouseAppApp')
+  .controller('TimepickerCtrl', ["auth", "$scope", "$location", "$log", function (auth, $scope, $location, $log) {
+
+    $scope.logout = function () {
+      auth.$signOut();
+      console.log('logged out');
+      $location.path('/login');
+      $scope.authData = null;
+    };
+
+    $scope.mytime = new Date();
+
+  $scope.hstep = 1;
+  $scope.mstep = 15;
+
+  $scope.options = {
+    hstep: [1, 2, 3],
+    mstep: [1, 5, 10, 15, 25, 30]
+  };
+
+  $scope.ismeridian = true;
+  $scope.toggleMode = function() {
+    $scope.ismeridian = ! $scope.ismeridian;
+  };
+
+  $scope.update = function() {
+    var d = new Date();
+    d.setHours( 14 );
+    d.setMinutes( 0 );
+    $scope.mytime = d;
+  };
+
+  $scope.changed = function () {
+    $log.log('Time changed to: ' + $scope.mytime);
+  };
+
+  $scope.clear = function() {
+    $scope.mytime = null;
+  };
+
+  }]);
---
+++ test/spec/controllers/timepicker.js
@@ -0,0 +1,22 @@
+'use strict';
+
+describe('Controller: TimepickerCtrl', function () {
+
+  // load the controller's module
+  beforeEach(module('myHouseAppApp'));
+
+  var TimepickerCtrl,
+    scope;
+
+  // Initialize the controller and a mock scope
+  beforeEach(inject(function ($controller, $rootScope) {
+    scope = $rootScope.$new();
+    TimepickerCtrl = $controller('TimepickerCtrl', {
+      $scope: scope
+    });
+  }));
+
+  it('should attach a list of awesomeThings to the scope', function () {
+    expect(scope.awesomeThings.length).toBe(3);
+  });
+});
